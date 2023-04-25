0d2794f4595702a3b3561aa57a9b333999e2b385
Mithilesh982 <127495653+Mithilesh982@users.noreply.github.com>
2023-04-25 15:59:09 +0530

    Update Gallery.js

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -24,27 +24,27 @@ export default function Gallery() {
             <div className="row gal-row text-center" >
                 <div className="col col-xl-12 bor p-4"  >
                     <div className="row text-light justify-content-center">
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0093.jpg" className='img-fluid w-75 w-md-100' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0104.jpg" className='img-fluid w-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0097.jpg" className='img-fluid w-75' alt="" /></div>       
-                    </div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0093.jpg" className='img-fluid w-75 w-md-100' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0104.jpg" className='img-fluid w-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0097.jpg" className='img-fluid w-75' alt="" /></div>       
+                    {/* </div> */}
 
-                    <div className="row text-light justify-content-center">
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0102.jpg" className='img-fluid w-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0112.jpg" className='img-fluid w-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0113.jpg" className='img-fluid w-75' alt="" /></div>
-                    </div>
+                    {/* <div className="row text-light justify-content-center"> */}
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0102.jpg" className='img-fluid w-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0112.jpg" className='img-fluid w-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0113.jpg" className='img-fluid w-75' alt="" /></div>
+                    {/* </div> */}
 
-                    <div className="row text-light justify-content-center">        
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0130.jpg" className='img-fluid w-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0131.jpg" className='img-fluid w-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0017.jpg" className='img-fluid w-75' alt="" /></div>
-                    </div>
+                    {/* <div className="row text-light justify-content-center">         */}
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0130.jpg" className='img-fluid w-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0131.jpg" className='img-fluid w-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0017.jpg" className='img-fluid w-75' alt="" /></div>
+                    {/* </div> */}
 
-                    <div className="row text-light justify-content-center">        
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0020.jpg" className='img-fluid w-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/DSC_0128.jpg" className='img-fluid w-75 h-75' alt="" /></div>
-                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-4 col-sm-12 py-4 f-col "><img src="images/front2.jpeg" className='img-fluid w-75 ' alt="" /></div>
+                    {/* <div className="row text-light justify-content-center">         */}
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0020.jpg" className='img-fluid w-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/DSC_0128.jpg" className='img-fluid w-75 h-75' alt="" /></div>
+                        <div data-aos="zoom-in-up" data-aos-delay="100" className="col col-xl-4 col-lg-4 col-md-6 col-sm-12 py-4 f-col "><img src="images/front2.jpeg" className='img-fluid w-75 ' alt="" /></div>
                     </div>
 
 
