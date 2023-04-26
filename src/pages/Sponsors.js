4b68f7b54a909e8e39c7b21c6b66385f6944544b
Mithilesh982 <127495653+Mithilesh982@users.noreply.github.com>
2023-04-26 12:52:09 +0530

    Update Sponsors.js

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -17,11 +17,11 @@ export default function Sponsors() {
     <Navbar/>
 
     <div className="container-fluid px-0">
-      <div className="row position-relative px-0 bg-dark" style={{filter:"blur(5px)" , marginTop:"-10%"}} ><img src="images/sponsor.jpg" className='px-0 mx-0 opacity-50' alt="" /></div>
-      <div className="row position-absolute a-pa justify-content-center " style={{marginTop:"-30%"}}>
-        <div className="row w-100 text-center"><h1>Sponsors</h1></div>
+      <div className="row position-relative px-0 mx-0 bg-dark" style={{filter:"blur(5px)" , marginTop:"-10%"}} ><img src="images/sponsor.jpg" className='px-0 mx-0 opacity-50' alt="" /></div>
+      <div className="row position-absolute a-pa justify-content-center px-0 mx-0 " style={{marginTop:"-30%"}}>
+        <div className="row w-100 text-center px-0 mx-0"><h1>Sponsors</h1></div>
 
-        <div className="row px-5 py-5 align-items-center m-col text-light" style={{backgroundColor:"black" }}> 
+        <div className="row px-5 py-5 align-items-center m-col text-light mx-0" style={{backgroundColor:"black" }}> 
             <div className="col col-xl-12 col-lg-12 col-md-12 col-sm-12 quote text-white text-center"><h2 style={{fontSize:"2.5vw"}} > " EMPOWERED BY YOUR SUPPORT " </h2></div>
             <div className="col col-xl-4 col-lg-4 col-md-4 col-sm-4 p-5  "><img src="images/crossHead.jpeg" className='w-75' alt="" /></div>
             <div className="col col-xl-4 col-lg-4 col-md-4 col-sm-4 p-5  "><img src="images/ansys.jfif" className='img-fluid' alt="" /></div>
