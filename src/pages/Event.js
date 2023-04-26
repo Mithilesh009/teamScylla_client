cb1a39fdf50e998a79cbeb51cceaadc4aad7df32
Mithilesh982 <127495653+Mithilesh982@users.noreply.github.com>
2023-04-26 16:19:22 +0530

    json added

--- src/pages/Event.js
+++ src/pages/Event.js
@@ -4,6 +4,8 @@ import Aos from 'aos'
 import 'aos/dist/aos.css'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import Data from '../data.json'
+
 
 export default function Event() {
 
@@ -11,12 +13,14 @@ export default function Event() {
    Aos.init ({duration:2000})
   },[])
 
+  console.log(Data)
+  console.log(Data[0].event[0].banner)
   return (
     <>
     <Navbar/>
 
     <div className="container-fluid bg-dark">
-        <div className="row img-row  "><img src="images/event.jpg" className='px-0 mx-0' alt="" /></div>
+        <div className="row img-row  "><img src={Data[0].event[0].banner} className='px-0 mx-0' alt="" /></div>
         <div className="row ab-row-2 justify-content-center w-100 ">
             <div className="col main-col-2 col-xl-12 ">
                 <div className="row text-center py-4"><h1>OUR JOURNEY</h1></div>
@@ -24,7 +28,7 @@ export default function Event() {
             <div className="row card-row text-center p-5" >
 
             <div className="row justify-content-center">
-                <div className="col col-xl-5 col-lg-5 col-md-5 col-sm-12 order-xl-1 order-lg-1 order-md-1 order-sm-2"><img src="images/Picture6.jpg" className='img-fluid' alt="" /></div>
+                <div className="col col-xl-5 col-lg-5 col-md-5 col-sm-12 order-xl-1 order-lg-1 order-md-1 order-sm-2"><img src={Data[0].event[0].isnee_img} className='img-fluid' alt="" /></div>
                 <div className="col col-xl-7 col-lg-7 col-md-7 col-sm-12 order-xl-2 order-lg-2 order-md-2 order-sm-1 text-start text-light p-xl-5 p-lg-4 p-md-3 p-sm-1">
                     <h2 style={{color:"rgb(211, 153, 7)"}} >ISNEE MOTORSPORTS</h2>
                     <h5 className='my-xl-5 my-lg-4 my-md-3 my-ms-1'>The journey of Team Scylla began way back in 2017 when a group of 25 undergraduates dared to do something different. They came up with an idea of forming a team which will build an All-Terrain Vehicle (ATV) to compete in the prestigious competition of Quad Torc.</h5></div>
@@ -36,7 +40,7 @@ export default function Event() {
                                 <div className="card w-75 mx-auto ">
                                 <div className="card-body  " >
                                     <div className="card-title"><h2>SHADOW 3.0 (2021-22)</h2></div>
-                                    <img src="images/_MG_4215.jpg" className='card-img w-75 border rounded' alt="" />
+                                    <img src={Data[0].event[0]['shadow3.0']} className='card-img w-75 border rounded' alt="" />
                                     <p className="card-text w-75 mx-auto abw2">Team participated in 8th season of ISNEE Quad Torc Competition. This season held from the 27th -  31th july 2022 at SIT pullor, Tamil Nadu. Team clear the technical inspection and qualified  for main event. Team successfully participated in the event and completed all . </p>
                                     </div>
                                 </div>
@@ -46,7 +50,7 @@ export default function Event() {
                                 <div className="card w-75 mx-auto">
                                 <div className="card-body  " >
                                     <div className="card-title"><h2>SHADOW 2.0 (2018-19)</h2></div>
-                                    <img src="images/Picture5.jpg" className='card-img w-75 border rounded' alt="" />
+                                    <img src={Data[0].event[0]['shadow2.0']} className='card-img w-75 border rounded' alt="" />
                                     <p className="card-text w-75 mx-auto abw2">Team participated in sixth season of ISNEE Quad Torc Competition. The sixth season held from the 7th -12th September 2019 at Garage1 Race Track chhachhri teep, Bijnor. Team clear the technical inspection and qualified  for main event. Team successfully participated all event.</p>
                                     </div>
                                 </div>
@@ -57,7 +61,7 @@ export default function Event() {
                                 <div className="card  w-75 mx-auto ">
                                 <div className="card-body  " >
                                     <div className="card-title"><h2>SHADOW 1.0 (2017-18)</h2></div>
-                                    <img src="images/Picture1.jpg" className='card-img border rounded' alt="" />
+                                    <img src={Data[0].event[0]['shadow1.0']} className='card-img border rounded' alt="" />
                                     <p className="card-text abw2"> Team participated in  fifth season of ISNEE Quad Torc Competition. The fifth season held from the 26th -30th September 2018 at Garage1 Race Track chhachhri teep, Bijnor. Team clear the technical inspection and qualified  for main event. Team successfully participated all event.  </p>
                                     </div>
                                 </div>
@@ -75,45 +79,6 @@ export default function Event() {
         </div>
     </div>
 
-
-{/* <div className="container-fluid bg-dark p-5">
-    <div className="row p-5">
-      <div className="col col-xl-12 col-lg-12 col-md-12 col-sm-12 text-center text-light">
-        <img src="images/logoScylla-W.png" width="70px" alt="" />
-        <h2 >TEAM SCYLLA RACING</h2>
-        <h6>" PERFORMANCE OVER EVERYTHING "</h6>
-        <hr style={{color:"white" , fontWeight:"bolder"}}/>
-      </div>
-      <div className="col col-xl-8 col-lg-6 col-md-6 col-sm-12 text-center text-light offset-xl-2 offset-sm-0 ">
-    
-        <h1 className='pb-5'>OUR JOURNEY</h1>
-        <p>The journey of Team Scylla began way back in 2017 when a group of 25 undergraduates dared to do something different. They came up with an idea of forming a team which will build an All-Terrain Vehicle (ATV) to compete in the prestigious competition of Quad Torc.</p>
-      </div>
-    </div>
-
-    <div className="row text-light text-center p-xl-5 p-lg-5 p-md-3 p-sm-2 bg-dark " >
-                    <div className="col col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-3">
-                        <div className="row ">
-                            
-                        </div>
-                        <div className="row m-1">
-                            
-                            <div className="col col-xl-6 col-lg-6 col-md-6 col-sm-12">
-                                <div className="card mx-auto p-3 bg-dark">
-                                    <div className="card-body  " >
-                                        <div className="card-title"><h2>SHADOW 3.0 (2021-22)</h2></div>
-                                        <img src="images/Picture7.jpg" className='card-img  border rounded' alt="" />    
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-
-</div> */}
-{/* <Footer/> */}
 
     </>
   )
